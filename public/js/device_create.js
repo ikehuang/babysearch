c3b9d86d8988e5e40fe3fe34cb22b0acf6cc190b
franky <franky@ink.net.tw>
2015-06-11 16:08:07 +0800

    bug checker id 1,2,4 fixed 0611v1

--- public/js/device_create.js
+++ public/js/device_create.js
@@ -267,7 +267,7 @@ $(document).ready(function() {
 			$("#ble").attr('src','/img/13/icon-03-01.png');
 			break;
 		default:
-			alert('序號格式不正確!');
+			//alert('序號格式不正確!');
 			return false;
 			break;
 	}
