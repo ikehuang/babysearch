2e6b561a8c3112b9578214f7bce35e7a7e01bdd9
franky <franky@ink.net.tw>
2015-07-02 14:53:00 +0800

    fixed device index tab

--- public/js/device_index.js
+++ public/js/device_index.js
@@ -1,24 +1,26 @@
 $(document).ready(function() {
 	
 	$("#notice_container").hide();
+	$('#petHealth_tab').css("background", "#4e65c5").css("border-bottom-left-radius", "1em").css("border-bottom-right-radius", "0em");
 	
-	$(".tab" ).click(function() {
-		if($(this).hasClass("notication_tab")) {
+	$(".pet_tab" ).click(function() {
+		if($(this).hasClass("pet_notication_tab")) {
 			$("#info_container").hide();
 			$("#notice_container").show();
 			
-			//$('#tab_container .notication_tab').attr('style', 'background-image:url("/img/02/bu-01-01.png");background-repeat:no-repeat;');
-			//$('#tab_container .info_tab').attr('style', 'background-image:url("/img/02/bu-02-01.png");background-repeat:no-repeat;');
-			//$('#info_background').attr('src', '/img/02/bu-02-01.png');
+			$('#petInfo_tab').css("background", "#4e88ff").css("border-bottom-right-radius", "1em").css("border-bottom-left-radius", "0em");
+			$('#petHealth_tab').css("background", "#4e65c5").css("border-bottom-left-radius", "1em").css("border-bottom-right-radius", "0em");
+			//$('.tab info_tab').css("background-image", "url('/img/02/bu-01-01.png') no-repeat").css("border-bottom-right-radius", "1em").css("border-bottom-left-radius", "0em");
+			//$('.tab notication_tab').css("background-image", "url('/img/02/bu-02-01.png') no-repeat").css("border-bottom-left-radius", "1em").css("border-bottom-right-radius", "0em");
 		}
 		else {
 			$("#info_container").show();
 			$("#notice_container").hide();
 			
-			//$('#tab_container .notication_tab').attr('style', 'background-image:url("/img/02/bu-01-02.png");background-repeat:no-repeat;');
-			//$('#tab_container .info_tab').attr('style', 'background-image:url("/img/02/bu-02-02.png");background-repeat:no-repeat;');
-			//$('#notice_background').attr('src', '/img/02/bu-01-02.png');
-			//$('#info_background').attr('src', '/img/02/bu-02-02.png');
+			$('#petHealth_tab').css("background", "#4e88ff").css("border-bottom-right-radius", "1em").css("border-bottom-right-radius", "0em");
+			$('#petInfo_tab').css("background", "#4e65c5").css("border-bottom-left-radius", "1em").css("border-bottom-left-radius", "0em");
+			//$('.tab info_tab').css("background-image", "url('/img/02/bu-02-02.png') no-repeat").css("border-bottom-right-radius", "1em").css("border-bottom-right-radius", "0em");
+			//$('.tab notication_tab').css("background-image", "url('/img/02/bu-01-02.png') no-repeat").css("border-bottom-left-radius", "1em").css("border-bottom-left-radius", "0em");
 		}
 		
 	});
