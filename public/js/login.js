eecfd4c73392fed97fc51d26b8ffecdd7dcc999d
franky <franky@ink.net.tw>
2015-07-03 19:10:40 +0800

    main page links and css added

---
+++ public/js/login.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+	$("#text1").hide();
+	$("#text2").hide();
+	$("#text3").hide();
+	$("#text4").hide();
+	$("#text5").hide();
+	$("#text6").hide();
+	$("#text7").hide();
+	$("#text8").hide();
+	
+	$("#link1" ).click(function() {
+		$("#text1").toggle();
+	});
+	$("#link2" ).click(function() {
+		$("#text2").toggle();
+	});
+	$("#link3" ).click(function() {
+		$("#text3").toggle();
+	});
+	$("#link4" ).click(function() {
+		$("#text4").toggle();
+	});
+	$("#link5" ).click(function() {
+		$("#text5").toggle();
+	});
+	$("#link6" ).click(function() {
+		$("#text6").toggle();
+	});
+	$("#link7" ).click(function() {
+		$("#text7").toggle();
+	});
+	$("#link8" ).click(function() {
+		$("#text8").toggle();
+	});
+});
